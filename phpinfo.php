48c92ff7b27292f4258bcd04c9091cb6d9d555dd
Santiago Acevedo <perez320@gmail.com>
2022-11-25 14:12:35 -0300

    Add files via upload

---
+++ phpinfo.php
@@ -0,0 +1,10 @@
+<?php
+
+// Mostra todas as informações, usa o padrão INFO_ALL
+phpinfo();
+
+// Mostra apenas informações dos módulos.
+// phpinfo(8) mostra um resultado identico.
+phpinfo(INFO_MODULES);
+
+?>
